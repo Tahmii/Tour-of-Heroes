831fa47a66486636c78ca9474bd4299ca318a19b
Tahmii <tahmi.ma222@gmail.com>
2022-12-12 23:27:18 +0100

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,12 +19,8 @@ import { MessagesComponent } from './messages/messages.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
-    HttpClientModule,
-
-    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
-    // and returns simulated server responses.
-    // Remove it when a real server is ready to receive requests.
+    AppRoutingModule,
+HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(
       InMemoryDataService, { dataEncapsulation: false }
     )
